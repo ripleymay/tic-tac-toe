6d2f3d5ad1a9c7b9859cfa53348521cb1a5e6bb1
Ripley Mayfield <ripleymay@Ripleys-MBP.attlocal.net>
2021-09-17 12:49:01 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*----- constants -----*/
+
+
+
+/*----- app's state (variables) -----*/
+let board;
+let turn;
+let win;
+
+
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
